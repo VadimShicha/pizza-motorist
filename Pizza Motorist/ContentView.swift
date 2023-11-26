0b0ccec2db1d36123624fdfe93c78bc452c41802
Vadim Shicha <shichavadim@gmail.com>
2023-11-26 11:45:11 -0800

    Created a title text

--- Pizza Motorist/ContentView.swift
+++ Pizza Motorist/ContentView.swift
@@ -10,12 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Pizza Motorist")
+                .font(.system(size: 45))
+                .padding(8)
+            Spacer()
         }
-        .padding()
     }
 }
 
