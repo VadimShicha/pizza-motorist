749dd2a37deca17368a22155c10d5bcbda03b37b
Vadim Shicha <shichavadim@gmail.com>
2023-11-25 17:57:26 -0800

    Initial Commit

---
+++ Pizza Motorist/Pizza_MotoristApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza_MotoristApp.swift
+//  Pizza Motorist
+//
+//  Created by Vadim Shicha on 11/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Pizza_MotoristApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
