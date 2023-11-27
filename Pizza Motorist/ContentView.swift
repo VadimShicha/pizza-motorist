8b45b761ecdca0da297168a096a76a05a25051ec
Vadim Shicha <shichavadim@gmail.com>
2023-11-26 23:50:45 -0800

    Car UI system and slider movement

--- Pizza Motorist/ContentView.swift
+++ Pizza Motorist/ContentView.swift
@@ -7,9 +7,25 @@
 
 import SwiftUI
 
+struct CarElement: Hashable {
+    var imageName = ""
+    
+    init(name: String) {
+        imageName = name
+    }
+}
+
+
+
 struct ContentView: View {
     @State private var currentWindowOpen: WindowType = WindowType.Home
     @State private var currentPopupOpen: PopupType = PopupType.None
+    
+    @State private var sliderValue: Double = 0
+    
+    @State private var carElements = [[CarElement]](); //array of array with Cars
+    
+    @State private var fallingVar: CGFloat = 0
     
     enum WindowType {
         case Home, MainGame
@@ -28,7 +44,28 @@ struct ContentView: View {
         }
     }
     
+    func setupGame() {
+        for _ in 1...10 {
+            carElements.append([CarElement(name: "RedCar"),CarElement(name: "RedCar"),CarElement(name: "RedCar"),CarElement(name: "RedCar"),CarElement(name: "RedCar")])
+        }
+    }
+
+    //changes the window while also setting up all the game elements
+    func startGame() {
+        currentWindowOpen = WindowType.MainGame
+        
+        if(carElements.count == 0) {
+            setupGame()
+        }
+    }
+    
+    let timer = Timer.publish(every: 0.0015, tolerance: 0.00015, on: .main, in: .common).autoconnect()
+    
+    let carWidth = 70.0
+    let carHeight = 109.375
+    
     var body: some View {
+
         if(currentWindowOpen == WindowType.Home) {
             ZStack {
                 VStack {
@@ -82,8 +119,8 @@ struct ContentView: View {
                     .contentShape(Rectangle())
                     .onTapGesture {
                         /* when the VStack is tapped (not a button on the stack),
-                        change the window */
-                        currentWindowOpen = WindowType.MainGame
+                        start the game */
+                        startGame()
                     }
                 
                 if(currentPopupOpen == PopupType.Quest) {
@@ -116,21 +153,59 @@ struct ContentView: View {
             }
         }
         else if(currentWindowOpen == WindowType.MainGame) {
-            VStack {
-                Spacer()
-                HStack {
-                    Spacer()
-                    Text("Main Game")
+            ZStack {
+                VStack {
+                    
+                }
+                .background(Color.blue)
+                //let cars: [Int] = [0, 1, 2, 3, 4]
+                
+                
+                VStack {
+                    ForEach(carElements, id: \.self) { carArray in
+                        HStack {
+                            ForEach(carArray, id: \.self) { car in
+                                Image(car.imageName)
+                                    .resizable()
+                                    .frame(width: carWidth, height: carHeight)
+                            }
+                        }
+                    }
                     Spacer()
                 }
+                .background(Color.gray)
+                .position(x: UIScreen.main.bounds.size.width / 2,
+                          y: UIScreen.main.bounds.size.height / 2 + carHeight + CGFloat(fallingVar))
+                //.padding(.top, CGFloat(fallingVar))
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    currentWindowOpen = WindowType.Home
+                }
                 
-                Spacer()
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        ZStack {
+                            Image("BlueCar")
+                                .resizable()
+                                .position(x: carWidth * sliderValue + (carWidth / 2))
+                                .frame(width: carWidth, height: carHeight)
+                            Slider(value: $sliderValue, in: -2...2, step: 1)
+                        }
+                        
+                        Spacer()
+                    }.padding(50)
+                }
+                .onReceive(timer) { time in
+                    fallingVar += 0.6
+                    
+                    if(fallingVar >= carHeight + 7.5) {
+                        fallingVar = 0
+                    }
+                }
             }
             .background(Color.blue)
-            .contentShape(Rectangle())
-            .onTapGesture {
-                currentWindowOpen = WindowType.Home
-            }
         }
     }
 }
